e0392a4b46a08fe67c88406ec7e0ac7cfea14a2a
Chris Barba <chrisbarba92@yahoo.com>
2022-08-28 19:28:25 -0700

    Fixing player choice and rounds

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
 
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random() * 3) + 1;
     
     if(compChoice == 1){
-    console.log("Computer chooses Rock " + compChoice);
     return compPick = "rock";
     }
     
     else if(compChoice == 2){
-    console.log("Computer chooses Paper " + compChoice);
     return compPick = "paper";
     }
 
     else{
-    console.log("Computer chooses Scissors " + compChoice);
     return compPick = "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
 
-    if(computerSelection == playerSelection)
-    console.log("Tie")
-    else if(computerSelection != playerSelection && computerSelection == playerSelection)
-    console.log("Lose")
-    else if(computerSelection == 3)
-    console.log("Win")
+    let log = "";
 
+    console.log("You picked " + playerSelection);
+    console.log("Computer picked " + computerSelection)
+
+    if((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+    log = "You Win";
+    
+    }
+    else if (playerSelection === computerSelection) {
+    log = "You Tied";
 }
-   
+    else
+    {
+    log = "You Lose";
+}
+    return log;
+}
+
+function game() {
+  
+
+        let roundResult = playRound(playerSelection, computerSelection);
+
+        if (roundResult.search('You Win') > -1) {
+          playerScore++;
+        } else if (roundResult.search('You Lose') > -1) {
+          computerScore++;
+        }
+     
+
+    console.log("Computer: " + computerScore);
+    console.log("Player: " + playerScore);
+}
+
+
+
+
   const playerSelection = prompt();
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+  console.log(game((playRound(playerSelection, computerSelection))));
